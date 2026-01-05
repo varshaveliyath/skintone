148c0d43ad7ad3501d9e60506be79641351df73c
unknown <veliyathvarsha@gmail.com>
2026-01-05 20:20:54 +0530

    domain change

--- backend/app.py
+++ backend/app.py
@@ -29,7 +29,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://skintone-lime.vercel.app",
+        "https://moodwear.vercel.app",
         "http://localhost:5173"
     ],
     allow_credentials=True,
